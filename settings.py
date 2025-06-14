0bd5ec84499f01a36d10e2e0a2aff55f8a8cd947
Chong Yao <chongyao.robin@gmail.com>
2025-06-14 10:37:28 -0500

    hide key

--- settings.py
+++ settings.py
@@ -1,6 +1,5 @@
 import redis
 
-HELIUS_API_KEY = "60ee1555-5946-477b-b61f-f5a9283e6a0f" # Move to cloud secret manager in prod env later
 HELIUS_MAIN_NET_NAME = "mainnet"
 HELIUS_DEV_NET_NAME = "devnet"
 
